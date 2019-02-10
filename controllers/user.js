6e71e004769135d65cc0780f9310dedfaae328c4
nguy4227 <36711210+nguy4227@users.noreply.github.com>
2019-02-10 15:25:51 -0800

    fixed photo size for grid

--- controllers/user.js
+++ controllers/user.js
@@ -58,7 +58,7 @@ exports.logout = (req, res) => {
   req.session.destroy((err) => {
     if (err) console.log('Error : Failed to destroy the session during logout.', err);
     req.user = null;
-    res.redirect('/');
+    res.redirect('/spectral');
   });
 };
 
